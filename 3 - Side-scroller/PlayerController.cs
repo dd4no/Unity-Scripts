e01e1bd62e6a939ebcceb0f9f051c0cfdc5f25fe
dd4no <74273226+dd4no@users.noreply.github.com>
2022-11-27 16:05:47 -0500

    Update PlayerController.cs
    Reordered code and added comments

--- 3 - Side-scroller/PlayerController.cs
+++ 3 - Side-scroller/PlayerController.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Objects
+    // Player Objects
     private Rigidbody playerBody;
-    private Animator playerAnimation;
-    private AudioSource playerAudio;
+    private Animator playerAnimator;
 
     // Actions
-    public float jumpForce = 10.0f;
-    public float gravityModifier = 1.0f;
+    private float jumpForce = 700.0f;
+    private float gravityModifier = 1.75f;
 
     // Particles
-    public ParticleSystem explosion;    
+    public ParticleSystem explosion;
     public ParticleSystem dirt;
 
-    // Sounds
-    public AudioClip jump;    
+    // Audio
+    private AudioSource soundEffects;
+    private AudioSource music;
+    public AudioClip jump;
     public AudioClip crash;
 
     // Flags
     public bool isOnGround = true;
     public bool gameOver = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Load objects
+        // Load Player Objects and Audio
         playerBody = GetComponent<Rigidbody>();
-        playerAnimation = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
+        playerAnimator = GetComponent<Animator>();
+        soundEffects = GetComponent<AudioSource>();
+        music = GameObject.Find("Main Camera").GetComponent<AudioSource>();
 
         // Modify gravity
         Physics.gravity *= gravityModifier;
     }
 
-    // Update is called once per frame
     void Update()
     {
         // If Spacebar is pressed with player in ground contact
@@ -45,42 +45,48 @@ public class PlayerController : MonoBehaviour
         {
             // Make player object jump
             playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
             // Indicate player object is not in ground contact
             isOnGround = false;
+
             // Play jump sound
-            playerAudio.PlayOneShot(jump, 1.0f);
+            soundEffects.PlayOneShot(jump, 1.0f);
+
             // Animate jump motion
-            playerAnimation.SetTrigger("Jump_trig");
+            playerAnimator.SetTrigger("Jump_trig");
             dirt.Stop();
         }
-        
+
     }
 
+    // Collisions
     private void OnCollisionEnter(Collision collision)
     {
         // If player object collides with ground
         if (collision.gameObject.CompareTag("Ground"))
         {
-            // Player object is in ground contact
+            // Indicate Player on Ground
             isOnGround = true;
+
             // Animate running particles
             dirt.Play();
         }
         // If player object collides with obstacle
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            // Game over
-            gameOver = true;
             // Indicate game over condition
+            gameOver = true;
             Debug.Log("Game Over!");
-            // Player is dead
-            playerAnimation.SetBool("Death_b", true);
-            playerAnimation.SetInteger("DeathType_int", 1);
+
             // Play crash sound
-            playerAudio.PlayOneShot(crash, 1.0f);
-            // Animate player death
-            dirt.Stop();
+            music.Stop();
+            soundEffects.PlayOneShot(crash, 1.0f);
+
+            // Animate Player Death
             explosion.Play();
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            dirt.Stop();
         }
     }
 }
